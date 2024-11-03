5446c89cb4bdaa7bed1120371c4c7dc50cb786b8
Affan <affanyounus91@gmail.com>
2024-11-03 18:53:45 +0500

    fix typo crash in RegisteredUserController

--- app/Http/Controllers/PatronControllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/PatronControllers/Auth/RegisteredUserController.php
@@ -61,7 +61,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);it
+        Auth::login($user);
 
         return response()->json($user, 201);
     }
